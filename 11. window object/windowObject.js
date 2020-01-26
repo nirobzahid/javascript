e4a5eacedd02c461df9f02f20b268525ffba5186
Zahid Hasan Nirob <60099993+nirobzahid@users.noreply.github.com>
2020-01-26 23:55:58 +0600

    Add files via upload

---
+++ 11. window object/windowObject.js
@@ -0,0 +1,72 @@
+//  alert('hello');
+// window.alert('hello world');
+
+// //prompt
+// const input = prompt();
+// alert(input);
+
+// //confirm
+// if( confirm('are you sure?')){
+// console.log('yes');
+// }else{
+//   console.log('No');
+// }
+
+//outer height and weight
+let val;
+let val1;
+val = window.outerWidth;
+console.log(val);
+val1 = window.outerHeight;
+console.log(val1);
+
+//inner height and weight
+let val2;
+let val3;
+val2 = window.innerWidth;
+console.log(val2);
+val3 = window.innerHeight;
+console.log(val3);
+
+//scroll
+let src;
+src = window.scrollY;
+
+//location address
+let locat;
+locat = window.location;
+console.log(locat);
+
+let pot;
+pot = window.location.port;
+pot = window.location.hostname;
+pot = window.location.href;
+pot = window.location.search;
+//pot = window.location.reload();
+console.log(pot);
+
+//history object
+//window.history.go(-1);
+let histry;
+histry = window.history.length;
+console.log(histry);
+
+//navigator object
+let nav = window.navigator;
+console.log(nav);
+
+let nav1 = window.navigator.appName;
+console.log(nav1);
+
+let nav2 = window.navigator.appVersion;
+console.log(nav2);
+
+let nav3 = window.navigator.platform;
+console.log(nav3);
+
+let nav4 = window.navigator.userAgent;
+console.log(nav4);
+
+let nav5 = window.navigator.vendor;
+console.log(nav5);
+
